09974b07f590e2c61a1680106fb2e0d3e6dce69c
pvyshnevetskyi <pvyshnevetskyi@griddynamics.com>
2022-11-11 21:44:42 +0200

    Stage 6/6 completed one more try

--- src/encryptdecrypt/cryptors/Cryptor.java
+++
@@ -1,7 +0,0 @@
-package encryptdecrypt.cryptors;
-
-public abstract class Cryptor {
-    public abstract String shift(String message, int key);
-
-    public abstract String unicode(String message, int key);
-}
